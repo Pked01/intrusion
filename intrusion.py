cf6fa828bf1cfb796cbf6007ce162fb619441a4f
Pked01 <khandelwalprateek01@gmail.com>
2018-04-20 13:55:55 +0530

    intrusion file

--- intrusion.py
+++ intrusion.py
@@ -169,4 +169,5 @@ class intrusion (object):
 
             
         
-        
+intr = intrusion('rtsp://192.168.20.9/6d801f1f-a9aa-449a-85d3-88608e5ee67b/6d801f1f-a9aa-449a-85d3-88608e5ee67b_vs1?token=6d801f1f-a9aa-449a-85d3-88608e5ee67b^LVERAMOTD^50^26^26^1657790795^d660cf85eebea453b0c933b63025aedeb9c22fea&username=admin',ROI=(228, 57, 149, 154))
+intr.detect_intrusion()
